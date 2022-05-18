9f503eb78719607a4db2a99492d343bd026dc066
oyjoh <oyvind.bre.joh@gmail.com>
2022-05-18 09:40:55 +0200

    update

--- app/externalresources/reader_LokiCastle.py
+++ app/externalresources/reader_LokiCastle.py
@@ -5,13 +5,20 @@ import xarray as xr
 
 
 class ReaderLokiCastle(NetcdfReaderInterface):
-    def __init__(self, id):
-        super().__init__(id)
+    def __init__(self, id=None):
+        super().__init__(id=1)
         self.path = 'app/externalresources/datasets/GS17_ROV01_LokiCastle_08072017_LLD_depthcorrected.grd'
         self.ds = None
 
     def get_full_dataset(self):
         return self.path
+
+    # sw, ne, (lat, lon)
+    def get_bounds(self):
+        lon = self.ds.lon.values
+        lat = self.ds.lat.values
+
+        return ((lat[0], lon[0]), (lat[-1], lon[-1]))
 
     def netcdf_to_dataarray(self):
         ds = xr.open_dataarray(self.path)
